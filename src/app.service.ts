89182afa4a553f3a43874b4fd2c0f26d0a1e2404
goodZ1k <vlad2001vol@gmail.com>
2021-12-22 19:53:58 +0200

    ver3

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,21 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  getNumbers(): number {
+    const x = 8;
+    const y = 3 + x;
+    return y;
+  }
+  getString(): string {
+    const name = "Vladislav";
+    return name;
+  }
+  getArrays(): number[] {
+    const list: number[] = [1,2,3];
+    return list;
+  }
+  
+
+
+
 }
